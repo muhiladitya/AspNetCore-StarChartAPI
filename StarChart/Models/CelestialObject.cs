151dcb2ede58e8c20483a18e50bdae947ab36d61
muhiladitya <56513895+muhiladitya@users.noreply.github.com>
2021-10-13 12:09:39 +0530

    Update CelestialObject.cs

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,11 @@ namespace StarChart.Models
 {
     public class CelestialObject
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? OrbitedObjectId { get; set; }
+        [NotMapped]
+        public List<CelestialObject> Satellites { get; set; }
+
     }
 }
